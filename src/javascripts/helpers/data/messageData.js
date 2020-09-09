b278de607c0cb38eb9dc353f77f48bbe5daf3784
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-09-08 23:15:07 -0500

    added array of objects for the initial messages that will be displayed in chat area.

---
+++ src/javascripts/helpers/data/messageData.js
@@ -0,0 +1,29 @@
+const initialMessages = [
+  {
+    quote: 'My precious.',
+    character: 'Gollum',
+  },
+  {
+    quote:
+      'I would rather share one lifetime with you than face all the Ages of this world alone.',
+    character: 'Arwen',
+  },
+  {
+    quote: 'Even the smallest person can chnage the course of the future.',
+    character: 'Galadriel',
+  },
+  {
+    quote:
+      'The board is set, the pieces are moving. We come to it at last, the great battle of our time.',
+    character: 'Gandalf',
+  },
+  {
+    quote:
+      'A day may come when the courage of men fails... but it is not THIS day.',
+    character: 'Aragorn',
+  },
+];
+
+const getInitialMessages = () => initialMessages;
+
+export default { getInitialMessages };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,10 @@
 import '../styles/main.scss';
+import SeedData from './helpers/data/messageData';
 
 const init = () => {
   $('#app').html('<h1>HELLO! You are up and running!</h1>');
   console.log('YOU ARE UP AND RUNNING!');
+  console.log(SeedData.getInitialMessages());
 };
 
 init();
