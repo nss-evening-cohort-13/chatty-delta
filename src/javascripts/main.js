18e32ee510ff53b3fcb590ddf6a6115aa670730f
Bailey <bailey.elise.dennis@gmail.com>
2020-09-12 12:23:05 -0500

    got button added and will switch, now to link up font sizing with components added by group

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,14 +1,15 @@
 import '../styles/main.scss';
 // import SeedData from './helpers/data/messageData';
-import themes from './components/modes';
+import modes from './components/modes';
 import container from './components/options-container';
 import multipleUsers from './components/multipleUsers';
 import navigation from './components/navigation';
 
 const init = () => {
   container.containerDom();
-  themes.themeDom();
-  themes.themeChange();
+  modes.themeDom();
+  modes.themeChange();
+  modes.fontChange();
   multipleUsers.dropDown();
   navigation.renderNavBar();
 };
