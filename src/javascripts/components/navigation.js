f0797fd35072dd85e75622ecf966b502a65d02a8
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-09-14 22:00:56 -0500

    styled both buttons

--- src/javascripts/components/navigation.js
+++ src/javascripts/components/navigation.js
@@ -10,7 +10,7 @@ const renderNavBar = () => {
             <form class="form-inline">
                 <div id="dropMenu"></div>
                 <input id="input-message" class="form-control mr-sm-2" type="text" placeholder="Enter message..." aria-label="Search">
-                <button id="submit-message" class="btn btn-success my-2 my-sm-0" type="button">Enter Message</button>
+                <button id="submit-message" class="enter-message" class="btn btn-success my-2 my-sm-0" type="button">Enter Message</button>
                 <div id = "error-message></div>
             </form>
         </nav>
