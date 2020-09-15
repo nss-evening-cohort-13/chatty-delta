3cbf95d8d6f0b28e7b398bc2e06c4aecb18d72a0
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-09-14 21:32:57 -0500

    delete button styled

--- src/javascripts/components/displayMessages.js
+++ src/javascripts/components/displayMessages.js
@@ -7,7 +7,7 @@ const messageDummy = (item) => {
     <div class="dummy">
       <h5 class="card-character">${item.character}</h5>
       <p class="card-quote">${item.quote}</p>
-      <button id="delete-${item.id}" type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i> Delete Message</button>
+      <button id="delete-${item.id}" type="button" class="delete-btn"><i class="far fa-trash-alt"></i> Delete Message</button>
       <p class="timeStamped">${item.time}</p>
       </div>
     </div>
